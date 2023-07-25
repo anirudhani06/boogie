ed2de5f9a9d998fb4040ed6c1c4211bb843b25fc
Anirudh <anirudhani300@gmail.com>
2023-07-25 17:03:05 +0530

    added bearer type

--- server/core/settings.py
+++ server/core/settings.py
@@ -128,6 +128,7 @@ REST_FRAMEWORK = {
     ],
 }
 SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Bearer",),
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
