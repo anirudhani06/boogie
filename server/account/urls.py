1c06a1b425cbc1991bfeec2bbfc931a6ea341748
Anirudh <anirudhani300@gmail.com>
2023-07-25 17:59:06 +0530

    added retrive user url

--- server/account/urls.py
+++ server/account/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
 router.register("", views.UserViewSet)
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("<str:id>/", views.UserViewSet.retrieve),
+]
