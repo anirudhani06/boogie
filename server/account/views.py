d8703263b0a0335546ac6f710fd62cc5b1f205d8
Anirudh <anirudhani300@gmail.com>
2023-07-25 16:50:27 +0530

    created user login

--- server/account/views.py
+++ server/account/views.py
@@ -7,9 +7,11 @@ from rest_framework.permissions import (
     AllowAny,
 )
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
-from .serializers import RegisterSerializer
-from django.contrib.auth import get_user_model
+from .serializers import RegisterSerializer, LoginSerializer
+from django.contrib.auth import get_user_model, authenticate
+
 
 USER = get_user_model()
 
@@ -29,11 +31,15 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action == "register":
             self.permission_classes = [AllowAny]
+        elif self.action == "login":
+            self.permission_classes = [AllowAny]
         return super().get_permissions()
 
     def get_serializer_class(self):
         if self.action == "register":
             return RegisterSerializer
+        elif self.action == "login":
+            return LoginSerializer
         return self.serializer_class
 
     @action(detail=False, methods=["POST"])
@@ -42,3 +48,30 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=["POST"])
+    def login(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        username = serializer.data.get("username")
+        password = serializer.data.get("password")
+
+        user = USER.objects.filter(username=username).first()
+
+        if user is None:
+            return Response(
+                {"message": "User not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            return Response(
+                {"message": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response(
+            {"refresh": str(refresh), "access": str(refresh.access_token)},
+            status=status.HTTP_200_OK,
+        )
