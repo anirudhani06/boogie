4409648b4f84bebbfd86215f4045650b3ea284ae
Anirudh <anirudhani300@gmail.com>
2023-07-25 15:22:59 +0530

    user model added in admin.py file

--- server/account/admin.py
+++ server/account/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+USER = get_user_model()
+
+
+@admin.register(USER)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "name", "email", "is_superuser", "avatar"]
+
+
+# Unregister your models here.
+admin.site.unregister(Group)
