269a7bbbfec971c580d70a46203c21d8204c3ce2
Anirudh <anirudhani300@gmail.com>
2023-07-31 12:30:33 +0530

    ceated place view set and list and create func added

--- server/place/views.py
+++ server/place/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics, status, views
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
+from .serializers import PlaceSerializer
+from .models import Place
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+USER = get_user_model()
+
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PlaceSerializer
+        elif self.action == "create":
+            return PlaceSerializer
+
+        return super().get_serializer_class()
+
+    def get_permissions(self):
+        if self.action == "list":
+            self.permission_classes = [IsAuthenticatedOrReadOnly]
+
+        elif self.action == "create":
+            self.permission_classes = [IsAuthenticated]
+
+        return super().get_permissions()
+
+    def list(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.queryset, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
