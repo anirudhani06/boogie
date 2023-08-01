60cc19287d9eb1d30f84bd24d9438a2fb475d693
Anirudh <anirudhani300@gmail.com>
2023-08-01 17:12:53 +0530

    user model added into admin pannel

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import User
 
 # Register your models here.
+admin.site.register(User)
+# Unregister your model here.
+admin.site.unregister(Group)
