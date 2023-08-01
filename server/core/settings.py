4267e1d1391454f5026a7b33db7cf108693e7430
Anirudh <anirudhani300@gmail.com>
2023-08-01 16:19:11 +0530

    added AUTH_USER_MODEL

--- server/core/settings.py
+++ server/core/settings.py
@@ -118,7 +118,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         # "rest_framework.authentication.SessionAuthentication",
