04f16a76e3a2f26e9c3a9e609e641ead1daed638
Anirudh <anirudhani300@gmail.com>
2023-08-04 13:40:18 +0530

    image serializer created and fixed multiple image fields

--- server/place/serializers.py
+++ server/place/serializers.py
@@ -17,9 +17,15 @@ class PerkSerializer(serializers.ModelSerializer):
         exclude = ("id", "created_at", "place")
 
 
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlaceImages
+        fields = ("id", "image")
+
+
 class PlaceSerializer(serializers.ModelSerializer):
     host = HostSerializer(read_only=True)
-    images = serializers.SerializerMethodField(read_only=True)
+    images = ImageSerializer(read_only=True, many=True)
     uploaded_images = serializers.ListField(
         child=serializers.ImageField(
             max_length=1000, allow_empty_file=False, use_url=False
@@ -32,8 +38,3 @@ class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = "__all__"
-
-    def get_images(self, obj):
-        return [
-            self.context["request"].build_absolute_uri(img) for img in obj.images.all()
-        ]
